7f32bf9a5bedabc5df848e09d09b787aaf337dda
erdoganishe <khotyanmisha@gmail.com>
2023-11-07 18:42:30 +0200

    login regpage

---
+++ public/script/regpage.js
@@ -0,0 +1,81 @@
+const translations = {
+    uk: {
+        registration: 'Реєстрація',
+        login: 'Логін',
+        email: 'Електронна пошта',
+        password: 'Пароль',
+        confirm: 'Підтвердження',
+        register: 'Зареєструватись',
+        haveAccount: 'Вже є аккаунт? Авторизація',
+        contactUs: "Зв'язатись з нами",
+        ourServices: 'Наші послуги',
+        privacyPolicy: 'Політика конфеденційості',
+        termsConditions: 'Умови користування'
+    },
+    en: {
+        registration: 'Registration',
+        login: 'Login',
+        email: 'Email',
+        password: 'Password',
+        confirm: 'Confirm',
+        register: 'Register',
+        haveAccount: 'Already have an account? Login',
+        contactUs: 'Contact us',
+        ourServices: 'Our Services',
+        privacyPolicy: 'Privacy Policy',
+        termsConditions: 'Terms & Conditions'
+    }
+};
+
+let currentLanguage = 'uk';
+
+function getTranslation(key) {
+    return translations[currentLanguage][key] || key;
+}
+
+function switchLanguage (){
+    if (currentLanguage == 'uk'){
+        currentLanguage = 'en';
+    }
+    else
+    {
+        currentLanguage = 'uk';
+    }
+}
+
+function updateText(){
+    document.getElementById('registraion-title').innerHTML = getTranslation('registration');
+    document.getElementById('login').placeholder = getTranslation('login');
+    document.getElementById('email').placeholder = getTranslation('email');
+    document.getElementById('password').placeholder = getTranslation('password');
+    document.getElementById('confirmPassword').placeholder = getTranslation('confirm');
+    document.getElementById('register').innerHTML = getTranslation('register');
+    document.getElementById('have-account').innerHTML = getTranslation('haveAccount');
+    document.getElementById('contact-us').innerHTML = getTranslation('contactUs');
+    document.getElementById('our-service').innerHTML = getTranslation('ourServices');
+    document.getElementById('private-policy').innerHTML = getTranslation('privacyPolicy');
+    document.getElementById('terms-cconditions').innerHTML = getTranslation('termsConditions');
+}
+
+function addOnClickEventForLanguageSwitch() {
+
+    var languageButtons = document.getElementsByClassName("flag-img");
+    for (let i=0;i<languageButtons.length;i++){
+        languageButtons[i].addEventListener("click", function () {
+            for (let j = 0; j < languageButtons.length; j++) {
+                languageButtons[j].classList.remove("choosen");
+            }
+            
+            languageButtons[i].classList.add("choosen");
+
+            switchLanguage();
+            updateText();
+        });
+    }
+
+    updateText();
+}
+
+
+addOnClickEventForLanguageSwitch();
+
