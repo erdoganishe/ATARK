6f07b90f908216aaa846aa07c184a9307bfb14f6
Erdoganishe <khotyanmisha@gmail.com>
2023-12-03 11:50:44 +0200

    lock editing added

--- public/script/lock.js
+++ public/script/lock.js
@@ -12,33 +12,32 @@ if (Object.values(availableLanguages).includes(urlParams.get('lang'))){
 
 const translations = {
     uk: {
-        newLock: 'Новий замок',
-        identificator: 'Ідентифікатор',
-        name: "Ім'я",
-        adress: 'Адреса',
-        image: 'Зображення',
-        addLock: 'Додати замок',
+        idLabel: 'Ідентифікатор',
+        nameLabel: "Ім'я",
+        placeholderName: "Введіть нове ім'я",
+        adressLabel: 'Адреса',
+        placeholderAdress: 'Введіть новy адресу',
+        saveChanges: 'Зберігти зміни',
+        change: 'Змінити', 
         contactUs: "Зв'язатись з нами",
         ourServices: 'Наші послуги',
         privacyPolicy: 'Політика конфеденційості',
         termsConditions: 'Умови користування',
-        imageOrDataError: 'Немає зображення або інформації'
     },
     en: {
-        newLock: 'New Lock',
-        identificator: 'Identificator',
-        name: 'Name',
-        adress: 'Adress',
-        image: 'Image',
-        addLock: 'Add Lock',
+        idLabel: 'ID',
+        nameLabel: 'Name',
+        placeholderName: 'Put new name',
+        adressLabel: 'Adress',
+        placeholderAdress: 'Put new adress',
+        saveChanges: 'Save changes',
+        change: 'Change', 
         contactUs: 'Contact us',
         ourServices: 'Our Services',
         privacyPolicy: 'Privacy Policy',
         termsConditions: 'Terms & Conditions',
-        imageOrDataError: 'Image or data missing'
     }
 };
-
 
 function getTranslation(key) {
     return translations[currentLanguage][key] || key;
@@ -55,13 +54,13 @@ function switchLanguage (){
 }
 
 function updateText(){
-    document.getElementById('new-lock-title').innerHTML = getTranslation('newLock');
-    document.getElementById('name-input-title').innerHTML = getTranslation('name');
-    document.getElementById('name-input').placeholder = getTranslation('name');
-    document.getElementById('adress-input-title').innerHTML = getTranslation('adress');
-    document.getElementById('adress-input').placeholder = getTranslation('adress');
-    document.getElementById('image-input-title').innerHTML = getTranslation('image');
-    document.getElementById('add-button-title').innerHTML = getTranslation('addLock');
+    document.getElementById('edit-lock-button').innerHTML = getTranslation('change');
+    document.getElementById('id-label').innerHTML = getTranslation('idLabel');
+    document.getElementById('name-label').innerHTML = getTranslation('nameLabel');
+    document.getElementById('name-input').placeholder = getTranslation('placeholderName');
+    document.getElementById('adress-label').innerHTML = getTranslation('adressLabel');
+    document.getElementById('adress-input').placeholder = getTranslation('placeholderAdress');
+    document.getElementById('save-changes-lock-button').innerHTML = getTranslation('saveChanges');
     document.getElementById('contact-us').innerHTML = getTranslation('contactUs');
     document.getElementById('our-service').innerHTML = getTranslation('ourServices');
     document.getElementById('private-policy').innerHTML = getTranslation('privacyPolicy');
@@ -83,3 +82,56 @@ function makeChosen() {
     updateText();
 
 }
+
+function blockInputs(){
+    const nameInput = document.getElementById('name-input');
+    const adressInput = document.getElementById('adress-input');
+
+    nameInput.disabled = true;
+    adressInput.disabled = true;
+}
+
+function unblockInputs(){
+    const nameInput = document.getElementById('name-input');
+    const adressInput = document.getElementById('adress-input');
+
+    nameInput.disabled = false;
+    adressInput.disabled = false;
+}
+
+function showButton(){
+    document.getElementById("save-changes-lock-button").classList.remove("hidden");
+}
+
+function hideButton(){
+    document.getElementById("save-changes-lock-button").classList.add("hidden");
+}
+
+function editButtonAddEvent(){
+    blockInputs();
+    const saveChanges = document.getElementById("save-changes-lock-button");
+    const editButton = document.getElementById("edit-lock-button");
+
+    editButton.addEventListener('click', async ()=>{
+
+        showButton();
+        unblockInputs();
+        editButton.classList.add("hidden");
+
+        saveChanges.addEventListener('click',()=>{
+            
+            const newName = document.getElementById('name-input').value;
+            const newAdress = document.getElementById('adress-input').value;
+            
+            blockInputs();
+            hideButton();
+            editButton.classList.remove("hidden");
+        });
+
+
+    });
+
+}
+
+makeChosen();
+editButtonAddEvent();
